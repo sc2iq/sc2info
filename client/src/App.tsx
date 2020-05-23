ed6344d67cc721ade0862af9eede42622c31008c
Matt Mazzola <mattmazzola@live.com>
2020-05-23 11:15:41 -0700

    remove redirect

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Route, Switch, NavLink, Redirect } from "react-router-dom"
+import { Route, Switch, NavLink } from "react-router-dom"
 import Search from './routes/Search'
 import Browse from './routes/Browse'
 
