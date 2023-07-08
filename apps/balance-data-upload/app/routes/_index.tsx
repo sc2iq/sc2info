cdf2a0a54993eb98293e303efcda6eb4f3a303c1
Matt Mazzola <mattmazzola@live.com>
2023-07-07 21:57:43 -0700

    Update to show link to uploaded blob

--- apps/balance-data-upload/app/routes/_index.tsx
+++ apps/balance-data-upload/app/routes/_index.tsx
@@ -4,10 +4,11 @@ import '../types.d.ts'
 import { useEffect, useRef, useState } from "react"
 import { ArrowPathIcon, ArrowUpOnSquareIcon } from '@heroicons/react/24/solid'
 import { containerClient } from "~/services/blobService"
+import { BlockBlobClient, BlockBlobUploadResponse } from "@azure/storage-blob"
+import { useMachine } from "@xstate/react"
+import { uploadStatusMachine } from "~/stateMachines/uploadStatusMachine"
 
 export const action = async ({ request }: ActionArgs) => {
-  console.log(`File Upload Action: `, { request })
-
   const uploadHandler = unstable_composeUploadHandlers(
     unstable_createFileUploadHandler({
       maxPartSize: 5_000_000,
@@ -23,16 +24,20 @@ export const action = async ({ request }: ActionArgs) => {
   )
 
   const balanceDataFiles = formData.getAll("files") as File[]
-  console.log({ balanceDataFiles })
+  if (balanceDataFiles.length > 0) {
+    const uploadResponses: Array<{ blockBlobClient: BlockBlobClient, response: BlockBlobUploadResponse }> = []
 
-  for (const balanceDataFile of balanceDataFiles) {
-    console.log({ balanceDataFile })
-    const filename = `balancedata_${Date.now()}.zip`
-    const fileBuffer = await balanceDataFile.arrayBuffer()
-    await containerClient.uploadBlockBlob(filename, fileBuffer, fileBuffer.byteLength)
+    for (const balanceDataFile of balanceDataFiles) {
+      console.log({ balanceDataFile })
+      const filename = `balancedata_${Date.now()}.zip`
+      const fileBuffer = await balanceDataFile.arrayBuffer()
+      const uploadResponse = await containerClient.uploadBlockBlob(filename, fileBuffer, fileBuffer.byteLength)
+      uploadResponses.push(uploadResponse)
+    }
 
+    console.log('Action: ', { uploadResponses })
     return {
-      'uploadstatus': 'success',
+      uploadResponses,
     }
   }
 
@@ -43,7 +48,13 @@ export default function Index() {
   const navigation = useNavigation()
   const folderPickerRef = useRef<HTMLInputElement>(null)
   const [files, setFiles] = useState<File[]>([])
+  const [uploadedBlobUrl, setUploadedBlobUrl] = useState<string>()
   const actionData = useActionData<typeof action>()
+  const [uploadMachineState, uploadMachineSend] = useMachine(uploadStatusMachine)
+  // Subscribe to the machine state
+  useEffect(() => {
+    console.log('Upload Machine State: ', { value: uploadMachineState.value })
+  }, [uploadMachineState])
 
   const folderPickerChange: React.ChangeEventHandler<HTMLInputElement> = (event) => {
     console.log(`Folder Picker Change: `, { event })
@@ -70,11 +81,13 @@ export default function Index() {
 
   // After form is submitted, reset the form
   useEffect(() => {
-    if (actionData?.uploadstatus === "success") {
+    const blobBlobClient = actionData?.uploadResponses?.at(0)?.blockBlobClient
+    if (blobBlobClient) {
+      setUploadedBlobUrl(blobBlobClient.url)
       folderPickerRef.current?.form?.reset()
       setFiles([])
     }
-  }, [actionData, files])
+  }, [actionData])
 
   return (
     <>
@@ -84,19 +97,19 @@ export default function Index() {
         <div className="flex gap-10 mb-5">
           <div className="flex flex-col gap-3">
             <div><b>1.</b> Open SC2Editor</div>
-            <img src="/images/step1_open_editor.png" alt="The icon for SC2 Editor" width="250" className="ring-2 ring-blue-400 ring-offset-slate-800 ring-offset-4 rounded-2xl" />
+            <img src="/images/step1_open_editor.png" alt="The icon for SC2 Editor" width="250" className="ring-2 ring-blue-400 ring-offset-slate-900 ring-offset-4 rounded-2xl" />
           </div>
           <div className="flex flex-col gap-3">
             <div><b>2.</b> Export Balance Data to Folder</div>
-            <img src="/images/step2_export_balance_data.png" alt="The file menu of SC2 editor application. Export Balance Data > Legacy of the Void" width="450" className="ring-2 ring-blue-400 ring-offset-slate-800 ring-offset-4 rounded-2xl" />
+            <img src="/images/step2_export_balance_data.png" alt="The file menu of SC2 editor application. Export Balance Data > Legacy of the Void" width="450" className="ring-2 ring-blue-400 ring-offset-slate-900 ring-offset-4 rounded-2xl" />
           </div>
           <div className="flex flex-col gap-3">
             <div><b>3.</b> Compress folder to .zip file</div>
-            <img src="/images/step3_compress_folder.png" alt="Right click menu on balance data folder with 'Compress as Zip file' highlighted" width="350" className="ring-2 ring-blue-400 ring-offset-slate-800 ring-offset-4 rounded-2xl" />
+            <img src="/images/step3_compress_folder.png" alt="Right click menu on balance data folder with 'Compress as Zip file' highlighted" width="350" className="ring-2 ring-blue-400 ring-offset-slate-900 ring-offset-4 rounded-2xl" />
           </div>
           <div className="flex flex-col gap-3">
             <div><b>4.</b> Upload .zip file</div>
-            <img src="/images/step4_upload_zip_file.png" alt="File explorer with .zip file highlighted" width="250" className="ring-2 ring-blue-400 ring-offset-slate-800 ring-offset-4 rounded-2xl" />
+            <img src="/images/step4_upload_zip_file.png" alt="File explorer with .zip file highlighted" width="250" className="ring-2 ring-blue-400 ring-offset-slate-900 ring-offset-4 rounded-2xl" />
           </div>
         </div>
         <Form method="post" encType="multipart/form-data" className="flex flex-col gap-8 items-center">
@@ -110,10 +123,10 @@ export default function Index() {
             onChange={folderPickerChange}
             required
             accept=".zip"
-            className="p-4 rounded-md bg-slate-200 ring-2 ring-blue-200 ring-offset-slate-800 ring-offset-4 border-none text-slate-800 font-semibold cursor-pointer"
+            className="p-4 rounded-md bg-slate-200 ring-2 ring-blue-200 ring-offset-slate-900 ring-offset-4 border-none text-slate-800 font-semibold cursor-pointer"
           />
           <div>
-            <button type="submit" className="flex flex-row gap-2 p-4 px-6 rounded-md bg-blue-500 ring-2 ring-blue-200 ring-offset-slate-800 ring-offset-4 border-none text-white font-semibold">
+            <button type="submit" className="flex flex-row gap-2 p-4 px-6 rounded-md bg-blue-500 ring-2 ring-blue-200 ring-offset-slate-900 ring-offset-4 border-none text-white font-semibold">
               <ArrowUpOnSquareIcon className="h-8 w-8 text-slate-100" />
               Upload
             </button>
@@ -144,6 +157,18 @@ export default function Index() {
               </ul>
             )}
         </div>
+        <div className="w-1/3 flex gap-4">
+          <div>Uploaded File:</div>
+          <a href={uploadedBlobUrl} className="text-blue-100 font-medium">{uploadedBlobUrl}</a>
+        </div>
+        <button onClick={() => { console.log('send toggle'); uploadMachineSend({ type: 'TOGGLE' }) }}>
+
+          {uploadMachineState.value === 'inactive'
+            ? 'Click to activate'
+            : 'Active! Click to deactivate'}
+        </button>
+        <pre>{uploadMachineState.value.toString()}</pre>
+        <pre>{JSON.stringify(uploadMachineState.toJSON(), null, 4)}</pre>
       </div>
     </>
   )
