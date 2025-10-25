e19b3f339118a1311066a3af82dccc063c1a9678
Matthew Mazzola <mattmazzola@live.com>
2025-10-25 20:22:42 +0000

    require AZURE_STORAGE_CONNECTION_STRING

--- apps/balance-data-upload/start.sh
+++ apps/balance-data-upload/start.sh
@@ -2,4 +2,9 @@
 
 set -ex
 
+if [ -z "$AZURE_STORAGE_CONNECTION_STRING" ]; then
+  echo "Error: AZURE_STORAGE_CONNECTION_STRING environment variable is required but not set."
+  exit 1
+fi
+
 npm run start
